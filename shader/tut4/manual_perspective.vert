cb7785c93f95c731e15be331b67c785c63bc5be1
Zhong Xiewei <zxwyue@gmail.com>
2017-06-16 21:02:53 +0800

    tut4

--- shader/tut4/manual_perspective.vert
+++ shader/tut4/manual_perspective.vert
@@ -8,18 +8,23 @@ smooth out vec4 outColor;
 uniform vec2 offset;
 uniform float fNear;
 uniform float fFar;
-uniform float frustumScale;
+uniform float xfrustumScale;
+uniform float yfrustumScale;
 
 void main()
 {
-    vec4 cameraPos = position + vec4(offset.x, offset.y, 0, 0);
+    vec4 cameraPos = position + vec4(offset.x, offset.y, 0.0f, 0.0f);
     vec4 clipPos;
-    clipPos.xy = cameraPos.xy*frustumScale;
+    clipPos.x = cameraPos.x*xfrustumScale;
+    clipPos.y = cameraPos.y*yfrustumScale;
 
     clipPos.z = cameraPos.z*(fNear+fFar)/(fNear-fFar);
-    clipPos.z += 2*fNear*fFar/(fNear-fFar);
+    clipPos.z = clipPos.z + 2.0f*fNear*fFar/(fNear-fFar);
 
     clipPos.w = -cameraPos.z;
+
+
+    clipPos.z = 0.0; // In the example the z is out of the range [-1,1]
 
     gl_Position = clipPos;
     outColor = color;
--- src/tut4/manual_perspective.cpp
+++ src/tut4/manual_perspective.cpp
@@ -10,8 +10,10 @@ int windowHeight = 500;
 
 GLSLProgram program;
 GLuint positionBufferObject;
+GLuint vao;
+float fScale=1.0;
 const float vertexPositions[] = {
-	 0.25f,  0.25f, -1.25f, 1.0f,
+    0.25f,  0.25f, -1.25f, 1.0f,
 	 0.25f, -0.25f, -1.25f, 1.0f,
 	-0.25f,  0.25f, -1.25f, 1.0f,
 
@@ -57,9 +59,7 @@ const float vertexPositions[] = {
 
 	 0.25f, -0.25f, -2.75f, 1.0f,
 	-0.25f, -0.25f, -2.75f, 1.0f,
-	-0.25f, -0.25f, -1.25f, 1.0f,
-
-
+    -0.25f, -0.25f, -1.25f, 1.0f,
 
 
 	0.0f, 0.0f, 1.0f, 1.0f,
@@ -117,10 +117,19 @@ char fragFilename[] = "../shader/tut4/manual_perspective.frag";
 
 void InitializeVertexBuffer()
 {
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
     glGenBuffers(1, &positionBufferObject);
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,4,GL_FLOAT,GL_FALSE,0,0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1,4,GL_FLOAT,GL_FALSE,0,(void*)(sizeof(vertexPositions)/2));
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
 }
 
 void InitializeProgram()
@@ -135,6 +144,25 @@ void init()
     InitializeProgram();
     InitializeVertexBuffer();
 
+    program.UseProgram();
+
+    GLint offsetLoc = glGetUniformLocation(program.GetProgramID(), "offset");
+    glUniform2f(offsetLoc, 0.5f, 0.5f);
+
+    
+	GLint xfrustumScaleUnif = glGetUniformLocation(program.GetProgramID(), "xfrustumScale");
+	GLint yfrustumScaleUnif = glGetUniformLocation(program.GetProgramID(), "yfrustumScale");
+	GLint zNearUnif = glGetUniformLocation(program.GetProgramID(), "zNear");
+	GLint zFarUnif = glGetUniformLocation(program.GetProgramID(), "zFar");
+
+    glUniform1f(xfrustumScaleUnif, fScale);
+    glUniform1f(yfrustumScaleUnif, fScale);
+    glUniform1f(zNearUnif, 1.0f);
+    glUniform1f(zFarUnif, 3.0f);
+
+    glUseProgram(0);
+
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glFrontFace(GL_CW);
@@ -142,49 +170,14 @@ void init()
 
 void display()
 {
-    float cameraPos[4];
-    cameraPos[0] = vertexPositions[0] + 0.5;
-    cameraPos[1] = vertexPositions[1] + 0.5;
-    cameraPos[2] = vertexPositions[2];
-    cameraPos[3] = vertexPositions[3];
-
-    float clipPos[4];
-    clipPos[0] = cameraPos[0];
-    clipPos[1] = cameraPos[1];
-
-    clipPos[2] = cameraPos[2]*(1.0+3.0)/(1.f-3.f);
-    clipPos.z = cameraPos.z*(fNear+fFar)/(fNear-fFar);
-    clipPos.z += 2*fNear*fFar/(fNear-fFar);
-
-    clipPos.w = -cameraPos.z;
-
-    gl_Position = clipPos;
-
     glClearColor(0.0f,0.0f,0.0f,0.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     program.UseProgram();
 
-    GLint offsetLoc = glGetUniformLocation(program.GetProgramID(), "offset");
-    glUniform2f(offsetLoc, 0.5f, 0.5f);
-
-    
-	GLint frustumScaleUnif = glGetUniformLocation(program.GetProgramID(), "frustumScale");
-	GLint zNearUnif = glGetUniformLocation(program.GetProgramID(), "zNear");
-	GLint zFarUnif = glGetUniformLocation(program.GetProgramID(), "zFar");
-
-    glUniform1f(frustumScaleUnif, 1.0f);
-    glUniform1f(zNearUnif, 1.0f);
-    glUniform1f(zFarUnif, 3.0f);
-
-    glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(0,4,GL_FLOAT,GL_FALSE,0,0);
-    glVertexAttribPointer(1,4,GL_FLOAT,GL_FALSE,0,(void*)(sizeof(vertexPositions)/2));
+    glBindVertexArray(vao);
     glDrawArrays(GL_TRIANGLES, 0, 36);
-    glDisableVertexAttribArray(0);
-    glDisableVertexAttribArray(1);
+    glBindVertexArray(0);
     glUseProgram(0);
     glutSwapBuffers();
     glutPostRedisplay();
@@ -192,6 +185,17 @@ void display()
 
 void reshape(int w, int h)
 {
+    float xScale = fScale/(w/(float)h);
+    float yScale = fScale;
+    program.UseProgram();
+
+	GLint xfrustumScaleUnif = glGetUniformLocation(program.GetProgramID(), "xfrustumScale");
+	GLint yfrustumScaleUnif = glGetUniformLocation(program.GetProgramID(), "yfrustumScale");
+
+    glUniform1f(xfrustumScaleUnif, xScale);
+    glUniform1f(yfrustumScaleUnif, yScale);
+    glUseProgram(0);
+
     glViewport(0, 0, w, h);
 }
 
